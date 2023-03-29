dbcf8048f95eb460bb3626a85c54efd1656f4485
amariths <amar.mehdi@iths.se>
2023-03-29 14:47:31 +0200

    a

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "",
+  base: "/index.html",
   plugins: [vue()],
   resolve: {
     alias: {
